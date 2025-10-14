adf1fd53c6ba8a427aaf6aae2a067c8900e96cc0
Seth-Banker <125103042+Seth-Banker@users.noreply.github.com>
2025-10-13 21:30:26 -0700

    Fixed cross-platform

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 function tryRequire(p) { try { return require(p); } catch { return null; } }
 
 module.exports =
-  tryRequire(path.join(__dirname, 'prebuilds', `${process.platform}-${process.arch}`, 'javascript-zip-native.node')) ||
-  tryRequire(path.join(__dirname, 'build', 'Release', 'javascript-zip-native.node')) ||
+  tryRequire(path.join(__dirname, 'prebuilds', `${process.platform}-${process.arch}`, 'javascript-zip.node')) ||
+  tryRequire(path.join(__dirname, 'build', 'Release', 'javascript-zip.node')) ||
   // Helpful error if neither exists
   (() => { throw new Error('Native binary not found. Run `npm rebuild` or install a prebuilt package.'); })();
